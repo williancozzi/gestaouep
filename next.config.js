7a69349e640dbb1c72165a99be5d3091f70f9c8a
Gabriel Sousa <gabrielsousa@wiz.co>
2023-11-08 00:24:07 -0300

    changing dist build

--- next.config.js
+++ next.config.js
@@ -2,22 +2,5 @@
 
 module.exports = {
   trailingSlash: true,
-  // output: "standalone",
-  // webpack: (config) => {
-  //   config.plugins.push(
-  //     new webpack.EnvironmentPlugin({
-  //       NODE_ENV: "production",
-  //       apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  //       authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  //       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  //       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  //       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  //       messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  //       appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  //       nextAuthUrl: process.env.NEXTAUTH_URL,
-  //       nextAuthSecret: process.env.NEXTAUTH_SECRET,
-  //     })
-  //   );
-  //   return config;
-  // },
+  distDir: "build",
 };
